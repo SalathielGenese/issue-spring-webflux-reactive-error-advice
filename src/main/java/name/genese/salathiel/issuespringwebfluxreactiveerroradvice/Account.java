f9a1f8c7a0ad4b89303b647576e6bf5334dfe299
SalathielGenese <salathielgenese@gmail.com>
2021-10-11 21:46:21 +0400

    feat: model domain transfer

---
+++ src/main/java/name/genese/salathiel/issuespringwebfluxreactiveerroradvice/Account.java
@@ -0,0 +1,18 @@
+package name.genese.salathiel.issuespringwebfluxreactiveerroradvice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Account
+ *
+ * @author Salathiel @t salathiel@genese.name
+ * @since Oct 11, 2021 @t 21:02:33
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    private String name;
+}
