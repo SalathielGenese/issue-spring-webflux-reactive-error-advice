82b8c773fb8d84b301e5099921b741dc6e382136
SalathielGenese <salathielgenese@gmail.com>
2021-10-11 22:53:38 +0400

    feat: decorate POST /welcome with AspectJ

--- src/main/java/name/genese/salathiel/issuespringwebfluxreactiveerroradvice/IssueSpringWebfluxReactiveErrorAdviceApplication.java
+++ src/main/java/name/genese/salathiel/issuespringwebfluxreactiveerroradvice/IssueSpringWebfluxReactiveErrorAdviceApplication.java
@@ -1,13 +1,87 @@
 package name.genese.salathiel.issuespringwebfluxreactiveerroradvice;
 
-import org.springframework.boot.SpringApplication;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import javax.validation.ConstraintViolationException;
+import javax.validation.Path.Node;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+import static java.util.stream.StreamSupport.stream;
+import static org.springframework.boot.SpringApplication.run;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.ResponseEntity.status;
+import static reactor.core.publisher.Mono.just;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class IssueSpringWebfluxReactiveErrorAdviceApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(IssueSpringWebfluxReactiveErrorAdviceApplication.class, args);
+        run(IssueSpringWebfluxReactiveErrorAdviceApplication.class, args);
     }
 
+    /**
+     * Remap {@link ConstraintViolationException} around reactive web handler to
+     * <br>
+     * <br>
+     * <pre>
+     * HTTP 422
+     *
+     * [ ...{ String[] paths; String template; } ]
+     */
+    @Aspect
+    @Component
+    static class AroundReactiveWebHandler {
+        @Around("@annotation(org.springframework.web.bind.annotation.PostMapping)")
+        public Object aroundPostMapping(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+            // Catch both sync and reactive errors
+            try {
+                final Object proceed = proceedingJoinPoint.proceed();
+
+                if (proceed instanceof Mono) {
+                    try {
+                        return just(((Mono<?>) proceed).toFuture().get());
+                    } catch (ExecutionException executionException) {
+                        throw executionException.getCause();
+                    }
+                }
+
+                return proceed;
+            } catch (ConstraintViolationException constraintViolationException) {
+                final List<Violation> violations = constraintViolationException.getConstraintViolations().stream()
+                        .map(violation -> new Violation(
+                                violation.getMessageTemplate(),
+                                stream(violation.getPropertyPath().spliterator(), false)
+                                        .map(Node::getName)
+                                        .collect(Collectors.toList())
+                        )).collect(Collectors.toList());
+
+                // NOTE: java.lang.ClassCastException: class org.springframework.http.ResponseEntity cannot be cast to class reactor.core.publisher.Mono
+                //       But if wrapped in a Mono ```return just( status(UNPROCESSABLE_ENTITY).body(violations) );```, the response becomes
+                //
+                //      < 200 OK OK
+                //      < Content-Type: [application/json]
+                //      < Content-Length: [159]
+                //
+                //      {"headers":{},"body":[{"template":"{javax.validation.constraints.NotNull.message}","path":["name"]}],"statusCode":"UNPROCESSABLE_ENTITY","statusCodeValue":422}
+                return status(UNPROCESSABLE_ENTITY).body(violations);
+            }
+        }
+
+        @Getter
+        @RequiredArgsConstructor
+        static class Violation {
+            private final String template;
+            private final List<String> path;
+        }
+    }
 }
