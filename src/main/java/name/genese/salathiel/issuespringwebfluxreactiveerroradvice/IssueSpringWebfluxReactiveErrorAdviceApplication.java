480e8a3569a4ffe65cc9bbea5cd7df336d03c697
SalathielGenese <salathielgenese@gmail.com>
2021-10-11 20:57:58 +0400

    initial commit

---
+++ src/main/java/name/genese/salathiel/issuespringwebfluxreactiveerroradvice/IssueSpringWebfluxReactiveErrorAdviceApplication.java
@@ -0,0 +1,13 @@
+package name.genese.salathiel.issuespringwebfluxreactiveerroradvice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueSpringWebfluxReactiveErrorAdviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueSpringWebfluxReactiveErrorAdviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/name/genese/salathiel/issuespringwebfluxreactiveerroradvice/IssueSpringWebfluxReactiveErrorAdviceApplicationTests.java
@@ -0,0 +1,13 @@
+package name.genese.salathiel.issuespringwebfluxreactiveerroradvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueSpringWebfluxReactiveErrorAdviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
