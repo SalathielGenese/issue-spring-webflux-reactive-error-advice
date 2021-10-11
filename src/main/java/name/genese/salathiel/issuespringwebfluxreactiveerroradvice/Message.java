7b561918f4599c9e5f774a2194053e13142023a6
SalathielGenese <salathielgenese@gmail.com>
2021-10-11 21:46:29 +0400

    feat: model response model

---
+++ src/main/java/name/genese/salathiel/issuespringwebfluxreactiveerroradvice/Message.java
@@ -0,0 +1,16 @@
+package name.genese.salathiel.issuespringwebfluxreactiveerroradvice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Message
+ *
+ * @author Salathiel @t salathiel@genese.name
+ * @since Oct 11, 2021 @t 21:42:06
+ */
+@Getter
+@AllArgsConstructor
+public class Message {
+    private final String content;
+}
